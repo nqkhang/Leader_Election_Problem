07d520c6d43b24fec576b5e2b787bab265db9fc4
Dual Phoe <nguyenquockhang117@gmail.com>
2020-12-18 23:51:10 +0100

    Update to new version, add new input, re-organize code flow, update node class

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,70 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-#include "My_Node.h"
-
+#include "node.h"
 #include <thread>
 #include <stdexcept>
 #include <unistd.h>
 
 using namespace std;
 
-void RunTest()
-{
-    My_Node a(3.14);
-
-    a.printDebug();
-}
-
-void LoadFromFile(const string iFileName,
-                  int &oNumberOfNode,
-                  vector<float> oValues)
-{
-    ifstream myfile(iFileName);
-    if (myfile.is_open())
-    {
-        string line;
-        getline(myfile, line);
-
-        oNumberOfNode = stoi(line);
-        
-        for (int i =0; i< oNumberOfNode; i++)
-        {
-            getline(myfile, line);
-            float temp = stof(line);
-            oValues.push_back(temp);
-        }
-
-        myfile.close();
-    }
-    else
-    {
-        cout << "Unable to open file\n";
-    }
-}
-
 int main()
 {
-    RunTest();
+    // initialization
+    reset_sent_messages();
 
-    vector<My_Node> nodes;
+    // prepare the array
+    int aNumberOfNode = 0;
+    int aMaxID =0;
+    vector<shared_ptr<Node>> nodes;
 
-    int aNumberOfNode =0;
-    vector<float> arrValue;
-    LoadFromFile("../input.txt", aNumberOfNode, arrValue );
+    setupNodes(nodes, aNumberOfNode, aMaxID);
 
-
-    for (float val : arrValue)
+    // connect the nodes
+    for (unsigned i = 0; i < aNumberOfNode - 1; ++i)
     {
-        //create new threads and pass the value here!
+        nodes[i]->connect(nodes[i + 1]);
     }
+
+    nodes[aNumberOfNode - 1]->connect(nodes[0]);
+
+    // print node info
+    cout << "\nNodes created:\n";
+    for (auto i: nodes)
+    {
+        i->printDebug();
+    }
+    cout << endl;
+
+    cout << "\n\nPress any key to start the election...";
+    string dummy;
+    cin >> dummy;
+
+    // prepare the futures to store the elected leader information
+    vector<future<int>> leaders;
+    for (auto i : nodes)
+    {
+        leaders.push_back(i->get_leader());
+    }
+    cout << endl;
+
+    // run the threads with the main node logic
+    for (auto i : nodes)
+    {
+        thread([](shared_ptr<Node> node) {  node->start_node_logic( ); }, i).detach();
+    }
+
+
+    // do the final synchronization (so that main doesn't end before the algorithm does)
+    for (unsigned i = 0; i < aNumberOfNode; i++)
+    {
+        if (leaders[i].get() != (int)aMaxID) //make sure every node have acknowledge the leader
+        {
+            throw runtime_error("Node didn't correctly detect it's leader.");
+        }
+    }
+
+    // final reports
+    cout << "\nAsynchronous run finished." << endl;
+    cout << "Total number of sent messages was : " << sent_messages() << endl;
 
     return 0;
 }
-
--- src/message.cpp
+++ src/message.cpp
@@ -1,3 +1,38 @@
 #include "message.h"
 
 // message::ImportFromJSONString()
+
+bool Message::isEmpty()
+{
+    if(_source_UID ==0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Message::clear()
+{
+    _isEmpty = true;
+    _source_UID = 0;
+    _type = "";
+    _value = 0;
+}
+
+void Message::printDebug()
+{
+    
+    cout <<"_source_UID: "<< _source_UID;
+    cout <<"\n_type: " << _type;
+    cout << "\n_value: " << _value << endl;
+}
+
+string Message::debugString()
+{
+    string res = "";
+    res += ("_source_UID: " + to_string(_source_UID));
+    res += ("\n_type: " + _type);
+    res += ("\n_value: " + to_string(_value));
+    res += "\n";
+    return res;
+}
